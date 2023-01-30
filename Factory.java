d338f6c285a8201b8a7000c1603a11d572fcf9a7
Griffinheats <99288944+Griffinheats@users.noreply.github.com>
2023-01-30 12:38:40 -0500

    added on to the main class

--- Factory.java
+++ Factory.java
@@ -10,20 +10,28 @@ public class Factory
         String models[] = {"green beans", "bananas"};
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        int pid = 1;
 
         System.out.println("Factory open\n");
         System.out.println("Enter the number of widgets for the next package or enter 0 to end: ");
         int numWidgets = sc.nextInt();
         while(numWidgets != 0)
         {
-            int wid = 1;
-            Package p = new Package("P" + pid, numWidgets);
-            pid++;
-            String color = "test";
-            Widget w = new Widget(color, "test", "P" + pid + "W" + wid);
-            wid++;
+            packageCounter++;
+            Package p = new Package("P" + packageCounter, numWidgets);
+            for(int i = 1; i < numWidgets; i++)
+            {
+                int numC = (int)(Math.random()*6);
+                String color = colors[numC];
+                int numM = (int)(Math.random()*4);
+                String model = models[numM];
+
+                Widget w = new Widget(color, model, "P" + packageCounter + "W" + i);
+            }
+            System.out.println(p.toString());
+            System.out.println("Enter the number of widgets for the next package or enter 0 to end: ");
+            numWidgets = sc.nextInt();
         }
         System.out.println("Factory closed.");
+        sc.close();
     }   
 }
